d9475b04a5a73cbcb3114843f7e7d6c8da97f14a
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-11-08 19:28:33 -0300

    criando repositorio e carregando arquivos iniciais

---
+++ src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Employee> list = new ArrayList<>();
+        String path = "e:\\temp\\funcionarios.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String employeeCsv = br.readLine();    //lê uma linha do arquivo e se o arquivo estiver no final, retorna nulo
+
+            while (employeeCsv != null){
+                String[] fields = employeeCsv.split(",");
+                list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+                employeeCsv = br.readLine();
+            }
+
+            Collections.sort(list);
+            for (Employee emp : list){
+                System.out.println(emp.getName() + "," + emp.getSalary());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
